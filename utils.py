f14774a811fea6daaeb513cb8159f1ed760e2f02
Majestic-dev <saararonmathias1@gmail.com>
2024-04-21 21:00:10 +0300

    Improve Button and TextInput class initialisation and some functions

--- utils.py
+++ utils.py
@@ -1,31 +1,48 @@
 import pygame
 
+from typing import Optional, Tuple
+
+
 class Button:
-    def __init__(self, x, y, width, height, text, font, font_color, bg_color, hover_color, border_color, border_width, screen):
+    def __init__(self, 
+                 x: int, 
+                 y: int, 
+                 width: int, 
+                 height: int, 
+                 font: pygame.font.Font,
+                 screen: pygame.display.set_mode, 
+                 text: Optional[str] = None,
+                 font_colour: Tuple[int, int, int] = (0, 0, 0), 
+                 bg_colour: Tuple[int, int, int] = (255, 255, 255),
+                 hover_colour: Tuple[int, int, int] = (128, 128, 128),
+                 border_colour: Tuple[int, int, int] = (0, 0, 0),
+                 border_width: int = 2
+                 ):
+        
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.text = text
         self.font = font
-        self.font_color = font_color
-        self.bg_color = bg_color
-        self.hover_color = hover_color
-        self.border_color = border_color
+        self.font_colour = font_colour
+        self.bg_colour = bg_colour
+        self.hover_colour = hover_colour
+        self.border_colour = border_colour
         self.border_width = border_width
         self.screen = screen
 
         self.surface = pygame.Surface((self.width, self.height))
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.text_surface = self.font.render(self.text, True, self.font_color)
+        self.text_surface = self.font.render(self.text, True, self.font_colour)
         self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
 
     def draw(self):
         if self.rect.collidepoint(pygame.mouse.get_pos()):
-            self.surface.fill(self.hover_color)
+            self.surface.fill(self.hover_colour)
         else:
-            self.surface.fill(self.bg_color)
-            pygame.draw.rect(self.surface, self.border_color, (0, 0, self.width, self.height), self.border_width)
+            self.surface.fill(self.bg_colour)
+            pygame.draw.rect(self.surface, self.border_colour, (0, 0, self.width, self.height), self.border_width)
 
         self.surface.blit(self.text_surface, self.text_rect)
         self.screen.blit(self.surface, (self.x, self.y))
@@ -35,32 +52,48 @@ class Button:
             return True
         return False
     
-    def set_text(self, text):
+    def set_text(self, text: str):
         self.text = text
-        self.text_surface = self.font.render(self.text, True, self.font_color)
+        self.text_surface = self.font.render(self.text, True, self.font_colour)
         self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
 
 class TextInput:
-    def __init__(self, x, y, width, height, font, font_color, bg_color, border_color, border_width, screen):
+    def __init__(self,
+                 x: int,
+                 y: int,
+                 width: int,
+                 height: int,
+                 font: pygame.font.Font,
+                 max_length: int,
+                 screen: pygame.display.set_mode,
+                 placeholder: Optional[str] = "",
+                 font_colour: Tuple[int, int, int] = (0, 0, 0),
+                 bg_colour: Tuple[int, int, int] = (255, 255, 255),
+                 border_colour: Tuple[int, int, int] = (0, 0, 0),
+                 border_width: int = 2
+                 ):
+        
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.font = font
-        self.font_color = font_color
-        self.bg_color = bg_color
-        self.border_color = border_color
+        self.max_length = max_length
+        self.placeholder = placeholder
+        self.font_colour = font_colour
+        self.bg_colour = bg_colour
+        self.border_colour = border_colour
         self.border_width = border_width
         self.screen = screen
 
         self.surface = pygame.Surface((self.width, self.height))
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.text_surface = self.font.render("", True, self.font_color)
+        self.text_surface = self.font.render(self.placeholder, True, self.font_colour)
         self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
 
     def draw(self):
-        self.surface.fill(self.bg_color)
-        pygame.draw.rect(self.surface, self.border_color, (0, 0, self.width, self.height), self.border_width)
+        self.surface.fill(self.bg_colour)
+        pygame.draw.rect(self.surface, self.border_colour, (0, 0, self.width, self.height), self.border_width)
 
         self.surface.blit(self.text_surface, self.text_rect)
         self.screen.blit(self.surface, (self.x, self.y))
@@ -69,22 +102,25 @@ class TextInput:
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             return True
         return False
-
-    def add_text(self, text):
-        current_font_size = self.font.size(' ')[1]
-
-        self.text_surface = self.font.render(text, True, self.font_color)
-        self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
-
-        while self.text_surface.get_width() > self.surface.get_width() and current_font_size > 10:
-            current_font_size -= 1
-            self.font = pygame.font.Font(None, current_font_size)
-            self.text_surface = self.font.render(text, True, self.font_color)
-            self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
     
-    def remove_text(self):
-        self.text_surface = self.font.render("", True, self.font_color)
-        self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
+    def add_text(self, text: str):
+        if len(text) < self.max_length:
+            current_font_size = self.font.size(' ')[1]
+
+            self.text_surface = self.font.render(text, True, self.font_colour)
+            self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
+
+            while self.text_surface.get_width() > self.surface.get_width():
+                current_font_size -= 1
+                self.font = pygame.font.Font(None, current_font_size)
+                self.text_surface = self.font.render(text, True, self.font_colour)
+                self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
+
+            while self.text_surface.get_width() < self.surface.get_width() * 0.9 and current_font_size < self.height:
+                current_font_size += 1
+                self.font = pygame.font.Font(None, current_font_size)
+                self.text_surface = self.font.render(text, True, self.font_colour)
+                self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width()/2, self.surface.get_height()/2))
 
     def get_text(self):
-        return self.text_surface.get_text()
+        return self.text_surface
