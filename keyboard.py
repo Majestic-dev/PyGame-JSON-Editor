28f801df8dd6695d32de86d9606d690cb3ecbc4b
Majestic-dev <saararonmathias1@gmail.com>
2024-04-24 11:27:16 +0300

    Move all the main event logic into a separate file into a separate class

---
+++ keyboard.py
@@ -0,0 +1,98 @@
+import pygame
+
+from utils import TextInput
+
+class Keyboard:
+    def __init__(
+            self,
+            text_input: TextInput = None,
+    ):
+        # Backspace variables
+
+        self.backspace_start_time = None
+        self.backspace_held = False
+        self.start_length = 0
+        self.letters_deleted = 0
+        self.text_input = text_input
+
+
+        # Mouse variables
+        self.input_box_active = False
+
+
+    def handle_backspace(self, keys, user_text: str):
+        self.start_length = len(user_text)
+        if keys[pygame.K_BACKSPACE]:
+            if self.backspace_start_time is None:
+                self.backspace_start_time = pygame.time.get_ticks()
+                
+            elif pygame.time.get_ticks() - self.backspace_start_time >= 600:
+                self.backspace_held = True
+
+            if self.backspace_held and pygame.time.get_ticks() - self.backspace_start_time >= 50:
+                if self.letters_deleted >= (self.start_length * 0.6):
+                    user_text = ""
+                    self.letters_deleted = 0
+                    self.start_length = 0
+                    self.text_input.add_text(user_text)
+
+                else:
+                    user_text = user_text[:-1]
+                    self.letters_deleted += 1
+                    self.text_input.add_text(user_text)
+        else:
+            self.backspace_start_time = None
+            self.backspace_held = False
+
+        return user_text
+    
+    def handle_mousedown(self, user_text: str, text_input, button, button_callback):
+        if self.text_input.is_clicked():
+            pygame.mouse.set_visible(False)
+            self.input_box_active = True
+            self.text_input.activated = True
+            if user_text == self.text_input.placeholder:
+                user_text = ""
+                text_input.add_text(user_text)
+
+        if button.is_clicked():
+            button_callback()
+
+        elif not self.text_input.is_clicked() and self.input_box_active:
+            self.input_box_active = False
+            self.text_input.activated = False
+            if user_text == "":
+                user_text = text_input.placeholder
+                self.text_input.add_text(user_text)
+
+        return user_text, self.input_box_active
+    
+    def handle_keydown(self, event, keys, user_text: str, text_input, text_input_callback):
+        if (keys[pygame.K_LCTRL] and keys[pygame.K_BACKSPACE]) or (keys[pygame.K_RCTRL] and keys[pygame.K_BACKSPACE]):
+            words = user_text.split(" ")
+            if len(words) > 1:
+                words = words[:-1]
+                user_text = " ".join(words)
+            else:
+                user_text = ""
+            text_input.add_text(user_text)
+
+        elif event.key == pygame.K_BACKSPACE:
+            user_text = user_text[:-1]
+
+        elif event.key != pygame.K_RETURN:
+            if len(user_text) < text_input.max_length:
+                user_text += event.unicode
+                self.start_length = len(user_text)
+
+        elif event.key == pygame.K_RETURN:
+            if user_text == "":
+                text_input.activated = False
+                user_text = text_input.placeholder
+                text_input.add_text(user_text)
+            else:
+                text_input_callback(user_text)
+                user_text = text_input.placeholder
+
+        text_input.add_text(user_text)
+        return user_text
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from utils import Button, TextInput
 
+from keyboard import Keyboard
+
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -30,13 +32,14 @@ text_input = TextInput(
             placeholder="Enter text here"
 )
 
+keyboard = Keyboard(
+            text_input=text_input
+)
+
+
 user_text = text_input.placeholder
 
 running = True
-
-backspace_held = False
-start_length = 0
-letters_deleted = 0
 
 input_box_active = False
 
@@ -51,66 +54,17 @@ while running:
     clock.tick(60)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_BACKSPACE]:
-        if backspace_start_time is None:
-            backspace_start_time = pygame.time.get_ticks()
-        elif pygame.time.get_ticks() - backspace_start_time >= 600 or backspace_held:
-            backspace_held = True
-            if pygame.time.get_ticks() - backspace_start_time >= 50:
-                if letters_deleted >= (start_length * 0.6):
-                    user_text = ""
-                    letters_deleted = 0
-                    start_length = 0
-                    text_input.add_text(user_text)
-                else:
-                    user_text = user_text[:-1]
-                    letters_deleted += 1
-                    text_input.add_text(user_text)
-    else:
-        backspace_start_time = None
+    user_text = keyboard.handle_backspace(keys, user_text)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_BACKSPACE:
-                backspace_held = False
-                letters_deleted = 0
-
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if text_input.is_clicked():
-                input_box_active = True
-                if user_text == text_input.placeholder:
-                    user_text = ""
-                    text_input.add_text(user_text)
-            if button.is_clicked():
-                button_callback()
-            elif not text_input.is_clicked() and input_box_active:
-                input_box_active = False
-                if user_text == "":
-                    user_text = text_input.placeholder
-                    text_input.add_text(user_text)
+            user_text, input_box_active = keyboard.handle_mousedown(user_text, text_input, button, button_callback)
 
         if event.type == pygame.KEYDOWN and input_box_active:
-            keys = pygame.key.get_pressed()
-            if (keys[pygame.K_LCTRL] and keys[pygame.K_BACKSPACE]) or (keys[pygame.K_RCTRL] and keys[pygame.K_BACKSPACE]):
-                user_text = ""
-                text_input.add_text(user_text)
-            if event.key == pygame.K_BACKSPACE:
-                user_text = user_text[:-1]
-            elif event.key != pygame.K_RETURN:
-                if len(user_text) < 20:
-                    user_text += event.unicode
-                    start_length = len(user_text)
-            elif event.key == pygame.K_RETURN:
-                if user_text == "":
-                    user_text = text_input.placeholder
-                    text_input.add_text(user_text)
-                else:
-                    text_input_callback(user_text)
-                    user_text = text_input.placeholder
-            text_input.add_text(user_text)
+            user_text = keyboard.handle_keydown(event, keys, user_text, text_input, text_input_callback)
     
     button.draw()
     text_input.draw()
