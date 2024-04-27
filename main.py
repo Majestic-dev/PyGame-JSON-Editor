800152692ab61d06e6ccf0c9b3d8760ccb2f1038
Majestic-dev <saararonmathias1@gmail.com>
2024-04-27 23:15:52 +0300

    Dynamic key buttons

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-from utils import Button, TextInput, DisplayJSONBox
+from utils import TextInput, DisplayJSONBox, DisplayJSONKeyButtonsDynamically
 
 from keyboard import Keyboard
 
@@ -10,16 +10,6 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("JSON Editor")
-
-button = Button(
-            x=250,
-            y=250,
-            width=150,
-            height=50,
-            font=pygame.font.Font(None, 24),
-            screen=screen,
-            text="Click Me!"
-)
 
 text_input = TextInput(
             x=125, 
@@ -46,6 +36,17 @@ keyboard = Keyboard(
             text_input=text_input
 )
 
+display_keys = DisplayJSONKeyButtonsDynamically(
+            x=15,
+            y=400,
+            width=750,
+            height=300,
+            font=pygame.font.Font(None, 24),
+            screen=screen,
+            button_width=750//5,
+            button_height=50,
+            display_json_box=text_box,
+)
 
 user_text = text_input.placeholder
 
@@ -71,19 +72,21 @@ while running:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            user_text, input_box_active = keyboard.handle_mousedown(user_text, text_input, button, button_callback)
+            user_text, input_box_active = keyboard.handle_mousedown(user_text, text_input)
 
         if event.type == pygame.KEYDOWN and input_box_active:
             user_text = keyboard.handle_keydown(event, user_text, text_input, text_input_callback, text_box)
         
         text_box.handle_event(event)
-    
-    button.draw()
+        display_keys.handle_event(event)
+
     text_input.draw()
     text_box.draw()
+    display_keys.draw()
 
     text_box.set_text("test.json")
-
+    display_keys.set_keys()
+    
     pygame.display.update()
     pygame.display.flip()
 
